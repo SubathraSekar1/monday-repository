1c3c83680d22f7e3e1d08f3ba2dc8b626fa1648f
SUBA <SUBA@DESKTOP-56GDHPQ>
2023-02-27 19:58:41 +0530

    commit

---
+++ Testing/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author SUBA
+ *
+ */
+module Testing {
+}
---
+++ Testing/src/simple/program.java
@@ -0,0 +1,9 @@
+package simple;
+
+public class program {
+
+	public static void main(String[] args) {
+		System.out.println("welcome");
+	}
+
+}
